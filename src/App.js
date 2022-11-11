939edbe7ab824b1062476002d52a4a1b6e597511
david63011 <david630@gmail.com>
2022-11-11 10:49:32 -0300

    add hooks

--- src/App.js
+++ src/App.js
@@ -2,20 +2,12 @@ import './App.css';
 import React from 'react';
 import Calculator from './components/Calculator';
 
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="cal-wrapper">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    
+      <Calculator />
+   
+  );
 }
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,51 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { total: null, next: null, operation: null };
-  }
+function Calculator() {
+  const [calc, setState] = useState({ total: null, next: null, operation: null });
 
-  math = (e) => {
+  const math = (e) => {
     const data = e.target.innerHTML;
-    const calcObj = calculate(this.state, data);
-    this.setState(calcObj);
+    setState((calc) => calculate(calc, data));
   };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="cal-wrapper">
-        <div className="display">
-            <div className="total">{total}</div>
-            <div className="next">{next}</div>
-            <div className="operation">{operation}</div>
-            </div>
-        <div className="grid">
-          <button className="cal-btn" type="button" onClick={this.math}>AC</button>
-          <button className="cal-btn" type="button" onClick={this.math}>+/-</button>
-          <button className="cal-btn" type="button" onClick={this.math}>%</button>
-          <button className="cal-btn right" type="button" onClick={this.math}>+</button>
-          <button className="cal-btn" type="button" onClick={this.math}>7</button>
-          <button className="cal-btn" type="button" onClick={this.math}>8</button>
-          <button className="cal-btn" type="button" onClick={this.math}>9</button>
-          <button className="cal-btn right" type="button" onClick={this.math}>x</button>
-          <button className="cal-btn" type="button" onClick={this.math}>4</button>
-          <button className="cal-btn" type="button" onClick={this.math}>5</button>
-          <button className="cal-btn" type="button" onClick={this.math}>6</button>
-          <button className="cal-btn right" onClick={this.math} type="button">-</button>
-          <button className="cal-btn" onClick={this.math} type="button">1</button>
-          <button className="cal-btn" type="button" onClick={this.math}>2</button>
-          <button className="cal-btn" type="button" onClick={this.math}>3</button>
-          <button className="cal-btn right" type="button" onClick={this.math}>÷</button>
-          <button className="cal-btn zero-btn" type="button" onClick={this.math}>0</button>
-          <button className="cal-btn" type="button" onClick={this.math}>.</button>
-          <button className="cal-btn right" type="button" onClick={this.math}>=</button>
-        </div>
+  const { total, next, operation } = calc;
+  return (
+    <div className="cal-wrapper">
+      <div className="display">
+       {total}{'  '}{operation}{'  '}{next}
       </div>
-    );
-  }
+      <div className="grid">
+        <button className="cal-btn" type="button" onClick={math}>AC</button>
+        <button className="cal-btn" type="button" onClick={math}>+/-</button>
+        <button className="cal-btn" type="button" onClick={math}>%</button>
+        <button className="cal-btn right" type="button" onClick={math}>+</button>
+        <button className="cal-btn" type="button" onClick={math}>7</button>
+        <button className="cal-btn" type="button" onClick={math}>8</button>
+        <button className="cal-btn" type="button" onClick={math}>9</button>
+        <button className="cal-btn right" type="button" onClick={math}>x</button>
+        <button className="cal-btn" type="button" onClick={math}>4</button>
+        <button className="cal-btn" type="button" onClick={math}>5</button>
+        <button className="cal-btn" type="button" onClick={math}>6</button>
+        <button className="cal-btn right" onClick={math} type="button">-</button>
+        <button className="cal-btn" onClick={math} type="button">1</button>
+        <button className="cal-btn" type="button" onClick={math}>2</button>
+        <button className="cal-btn" type="button" onClick={math}>3</button>
+        <button className="cal-btn right" type="button" onClick={math}>÷</button>
+        <button className="cal-btn zero-btn" type="button" onClick={math}>0</button>
+        <button className="cal-btn" type="button" onClick={math}>.</button>
+        <button className="cal-btn right" type="button" onClick={math}>=</button>
+      </div>
+    </div>
+  );
 } 
-export default Calculator;
 
+export default Calculator;
