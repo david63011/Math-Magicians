2732b7831b3bc7d578177b9ab3467334b116d3ea
david63011 <david630@gmail.com>
2022-11-16 11:15:17 -0300

    add new lintors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,25 @@
-import {Link, useMatch, useResolvedPath} from "react-router-dom"
-export default function Navbar () {
-    return (
-        <nav className="nav">
-        <h1 className= "title">Math Magicians</h1>
-        <ul>
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <h1 className="title">Math Magicians</h1>
+      <ul>
         <CustomLink to="/">Home</CustomLink>
         <CustomLink to="/calculator">Calculator</CustomLink>
         <CustomLink to="/quote">Quote</CustomLink>
-        </ul>
-       </nav>
-    );
+      </ul>
+    </nav>
+  );
 }
 
-function CustomLink({to, children, ...props}) {
-    const resolvedPath= useResolvedPath(to)
-    const match = useMatch({path: resolvedPath.pathname, end: true})
-    return (
-        <li className= {match ? "active" : ""}>
-        <Link to={to} {...props}>
-            {children}
-        </Link>
-        </li>
-    );
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const match = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={match ? 'active' : ''}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
 }
